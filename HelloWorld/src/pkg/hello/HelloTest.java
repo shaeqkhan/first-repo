99beda37c9bf8bce0a9dc3af874ad57b948bc585
Shaeq <shaeqkhan@gmail.com>
2014-12-12 22:24:51 -0500

    new class

---
+++ HelloWorld/src/pkg/hello/HelloTest.java
@@ -0,0 +1,10 @@
+package pkg.hello;
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TESTING MERGE IN GIT...");
+	}
+
+}
